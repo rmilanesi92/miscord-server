7e5c30066a5946f2a683e275e5e774abc02c9e3e
Rob <172938004+rmilanesi92@users.noreply.github.com>
2025-03-09 00:17:11 +0100

    Add base RespValue struct

---
+++ resp/resp_value.go
@@ -0,0 +1,17 @@
+package resp
+
+// Represent a RESP value
+type RespValue struct {
+    Kind byte
+    Value interface{}
+}
+
+// Simplified RespValue error constructor from error interface
+func NewError(err error) RespValue {
+    return RespValue{ Kind: ERR, Value: err.Error()}
+}
+
+// Simplified RespValue error constructor from error message 
+func NewErrorFromMsg(msg string) RespValue {
+    return RespValue{ Kind: ERR, Value: msg}
+}
